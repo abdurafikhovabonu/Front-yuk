90f699a2b669f81112bb24259424fef2ff776ab1
abdurafikhovabonu <abdurafikhovabonu@gmail.com>
2026-05-14 01:16:00 +0500

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,54 @@
+// export default defineNuxtConfig({
+//   compatibilityDate: '2025-07-15',
+//   modules: ['@nuxtjs/i18n'],
+//   i18n: {
+//     /** Til fayllari `frontend/locales/` da (default `i18n/locales` emas) */
+//     restructureDir: '.',
+//     locales: [
+//       { code: 'uz', language: 'uz-Latn-UZ', name: "O'zbekcha", file: 'uz.json' },
+//       { code: 'en', language: 'en-US', name: 'English', file: 'en.json' },
+//       { code: 'ru', language: 'ru-RU', name: 'Русский', file: 'ru.json' }
+//     ],
+//     defaultLocale: 'uz',
+//     lazy: true,
+//     langDir: 'locales',
+//     strategy: 'no_prefix',
+//     detectBrowserLanguage: {
+//       useCookie: true,
+//       cookieKey: 'i18n_locale',
+//       redirectOn: 'root',
+//       alwaysRedirect: false
+//     }
+//   },
+//   css: ['../assets/css/tailwind.css'],
+//   postcss: {
+//     plugins: {
+//       tailwindcss: {},
+//       autoprefixer: {},
+//     },
+//   },
+//     vite: {
+//     optimizeDeps: {
+//       include: [
+//         '@vue/devtools-core',
+//         '@vue/devtools-kit',
+//       ]
+//     }
+//   },
+//   devtools: { enabled: true },
+//   nitro: {
+//     preset: 'node-server'
+//   },
+//   app: {
+//     head: {
+//       title: 'Yukchi uz',
+//       meta: [
+//         { charset: 'utf-8' },
+//         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+//       ]
+//     }
+//   }
+// })
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   
@@ -22,7 +73,7 @@ export default defineNuxtConfig({
     }
   },
   
-  css: ['~/assets/css/tailwind.css'],  // ~/ assets ga ishora qiladi
+  css: ['~/assets/css/tailwind.css'],
   
   postcss: {
     plugins: {
@@ -42,12 +93,11 @@ export default defineNuxtConfig({
   
   devtools: { enabled: true },
   
-  // Netlify uchun muhim sozlamalar
+  // Vercel uchun muhim sozlamalar
   nitro: {
-    preset: 'netlify',  // netlify preset - muhim!
+    preset: 'vercel-edge',  // vercel preset
     output: {
-      dir: 'dist',      // Netlify default publish papkasi
-      serverDir: '.netlify/functions-internal'
+      dir: 'dist'
     },
     compressPublicAssets: true,
     minify: true
@@ -63,9 +113,6 @@ export default defineNuxtConfig({
     }
   },
   
-  // Netlify deploy tezligi uchun
   sourcemap: false,
-  
-  // Static generation (SEO uchun)
   ssr: true
 })
